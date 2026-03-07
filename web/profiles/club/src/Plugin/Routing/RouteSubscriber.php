97c3f47b56c1a54191927b191316d70811128913
nancy cole <nancycole29@gmail.com>
2026-03-07 13:17:24 -0500

    Deleted club_forum. Updated content moderation. Added gravel rides block.

---
+++ web/profiles/club/src/Plugin/Routing/RouteSubscriber.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Drupal\nscyc\Routing;
+
+use Drupal\Core\Routing\RouteSubscriberBase;
+use Symfony\Component\Routing\RouteCollection;
+
+/**
+  * Class RouteSubscriber.
+  *
+  * @package Drupal\nscyc\Routing
+  */
+class RouteSubscriber extends RouteSubscriberBase {
+
+ /**
+  * {@inheritdoc}
+  */
+  protected function alterRoutes(RouteCollection $collection) {
+
+    // Alter the path to the personal contact form .
+    if ($route = $collection->get('entity.user.contact_form')) {
+      $route->setPath('/contact/{user}/contact');
+    }
+
+  }
+}
